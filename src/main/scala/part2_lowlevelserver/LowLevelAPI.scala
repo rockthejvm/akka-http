b343f2e5dad9d8a54388020c0c0e87f954927a3a
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-03-09 21:04:28 +0000

    low level exercise and some other bits

--- src/main/scala/part2_lowlevelserver/LowLevelAPI.scala
+++ src/main/scala/part2_lowlevelserver/LowLevelAPI.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.IncomingConnection
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.Location
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink}
 
@@ -165,5 +166,64 @@ object LowLevelAPI extends App {
 
   // shorthand version
   Http().bindAndHandle(streamsBasedRequestHandler, "localhost", 8082)
+
+  /**
+    * Exercise: create your own HTTP server running on localhost on 8388, which replies
+    *   - with a welcome message on the "front door" localhost:8388
+    *   - with a proper HTML on localhost:8388/about
+    *   - with a 404 message otherwise
+    */
+
+  val syncExerciseHandler: HttpRequest => HttpResponse = {
+    case HttpRequest(HttpMethods.GET, Uri.Path("/"), _, _, _) =>
+      HttpResponse(
+        // status code OK (200) is default
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          "Hello from the exercise front door!"
+        )
+      )
+
+    case HttpRequest(HttpMethods.GET, Uri.Path("/about"), _, _, _) =>
+      HttpResponse(
+        // status code OK (200) is default
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            | <body>
+            |   <div style="color: red">
+            |     Hello from the about page!
+            |   <div>
+            | </body>
+            |</html>
+          """.stripMargin
+        )
+      )
+
+    // path /search redirects to some other part of our website/webapp/microservice
+    case HttpRequest(HttpMethods.GET, Uri.Path("/search"), _, _, _) =>
+      HttpResponse(
+        StatusCodes.Found,
+        headers = List(Location("http://google.com"))
+      )
+
+    case request: HttpRequest =>
+      request.discardEntityBytes()
+      HttpResponse(
+        StatusCodes.NotFound,
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          "OOPS, you're in no man's land, sorry."
+        )
+      )
+  }
+
+  val bindingFuture = Http().bindAndHandleSync(syncExerciseHandler, "localhost", 8388)
+
+  // shutdown the server:
+  bindingFuture
+    .flatMap(binding => binding.unbind())
+    .onComplete(_ => system.terminate())
 
 }
