2fcc83fe967ba181af1d77c7ab7c85cc90b604be
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-03-10 17:32:58 +0000

    low level server with query parameters

--- src/main/scala/part2_lowlevelserver/LowLevelRest.scala
+++ src/main/scala/part2_lowlevelserver/LowLevelRest.scala
@@ -3,10 +3,11 @@ package part2_lowlevelserver
 import akka.pattern.ask
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.Uri.Query
 import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
-import part2_lowlevelserver.GuitarDB.{CreateGuitar, FindAllGuitars, GuitarCreated}
+
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
@@ -14,13 +15,15 @@ import scala.concurrent.duration._
 // step 1
 import spray.json._
 
-case class Guitar(make: String, model: String)
+case class Guitar(make: String, model: String, quantity: Int = 0)
 
 object GuitarDB {
   case class CreateGuitar(guitar: Guitar)
   case class GuitarCreated(id: Int)
   case class FindGuitar(id: Int)
   case object FindAllGuitars
+  case class AddQuantity(id: Int, quantity: Int)
+  case class FindGuitarsInStock(inStock: Boolean)
 }
 
 class GuitarDB extends Actor with ActorLogging {
@@ -43,13 +46,31 @@ class GuitarDB extends Actor with ActorLogging {
       guitars = guitars + (currentGuitarId -> guitar)
       sender() ! GuitarCreated(currentGuitarId)
       currentGuitarId += 1
+
+    case AddQuantity(id, quantity) =>
+      log.info(s"Trying to add $quantity items for guitar $id")
+      val guitar: Option[Guitar] = guitars.get(id)
+      val newGuitar: Option[Guitar] = guitar.map {
+        case Guitar(make, model, q) => Guitar(make, model, q + quantity)
+      }
+
+      newGuitar.foreach(guitar => guitars = guitars + (id -> guitar))
+      sender() ! newGuitar
+
+    case FindGuitarsInStock(inStock) =>
+      log.info(s"Searching for all guitars ${if(inStock) "in" else "out of"} stock")
+      if (inStock)
+        sender() ! guitars.values.filter(_.quantity > 0)
+      else
+        sender() ! guitars.values.filter(_.quantity == 0)
+
   }
 }
 
 // step 2
 trait GuitarStoreJsonProtocol extends DefaultJsonProtocol {
   // step 3
-  implicit val guitarFormat = jsonFormat2(Guitar)
+  implicit val guitarFormat = jsonFormat3(Guitar)
 }
 
 object LowLevelRest extends App with GuitarStoreJsonProtocol {
@@ -57,10 +78,12 @@ object LowLevelRest extends App with GuitarStoreJsonProtocol {
   implicit val system = ActorSystem("LowLevelRest")
   implicit val materializer = ActorMaterializer()
   import system.dispatcher
+  import GuitarDB._
 
   /*
     - GET on localhost:8080/api/guitar => ALL the guitars in the store
-    POST on localhost:8080/api/guitar => insert the guitar into the store
+    X GET on localhost:8080/api/guitar?id=X => fetches the guitar associated with id X
+    - POST on localhost:8080/api/guitar => insert the guitar into the store
    */
 
   // JSON -> marshalling
@@ -72,7 +95,8 @@ object LowLevelRest extends App with GuitarStoreJsonProtocol {
     """
       |{
       |  "make": "Fender",
-      |  "model": "Stratocaster"
+      |  "model": "Stratocaster",
+      |  "quantity": 3
       |}
     """.stripMargin
   println(simpleGuitarJsonString.parseJson.convertTo[Guitar])
@@ -95,16 +119,84 @@ object LowLevelRest extends App with GuitarStoreJsonProtocol {
     server code
    */
   implicit val defaultTimeout = Timeout(2 seconds)
+
+  def getGuitar(query: Query): Future[HttpResponse] = {
+    val guitarId = query.get("id").map(_.toInt) // Option[Int]
+
+    guitarId match {
+      case None => Future(HttpResponse(StatusCodes.NotFound)) // /api/guitar?id=
+      case Some(id: Int) =>
+        val guitarFuture: Future[Option[Guitar]] = (guitarDb ? FindGuitar(id)).mapTo[Option[Guitar]]
+        guitarFuture.map {
+          case None => HttpResponse(StatusCodes.NotFound) // /api/guitar?id=9000
+          case Some(guitar) =>
+            HttpResponse(
+              entity = HttpEntity(
+                ContentTypes.`application/json`,
+                guitar.toJson.prettyPrint
+              )
+            )
+        }
+    }
+  }
+
+
   val requestHandler: HttpRequest => Future[HttpResponse] = {
-    case HttpRequest(HttpMethods.GET, Uri.Path("/api/guitar"), _, _, _) =>
-      val guitarsFuture: Future[List[Guitar]] = (guitarDb ? FindAllGuitars).mapTo[List[Guitar]]
-      guitarsFuture.map { guitars =>
-        HttpResponse(
-          entity = HttpEntity(
-            ContentTypes.`application/json`,
-            guitars.toJson.prettyPrint
+    case HttpRequest(HttpMethods.POST, uri@Uri.Path("/api/guitar/inventory"), _, _, _) =>
+      val query = uri.query()
+      val guitarId: Option[Int] = query.get("id").map(_.toInt)
+      val guitarQuantity: Option[Int] = query.get("quantity").map(_.toInt)
+
+      val validGuitarResponseFuture: Option[Future[HttpResponse]] = for {
+        id <- guitarId
+        quantity <- guitarQuantity
+      } yield {
+        val newGuitarFuture: Future[Option[Guitar]] = (guitarDb ? AddQuantity(id, quantity)).mapTo[Option[Guitar]]
+        newGuitarFuture.map(_ => HttpResponse(StatusCodes.OK))
+      }
+
+      validGuitarResponseFuture.getOrElse(Future(HttpResponse(StatusCodes.BadRequest)))
+
+    case HttpRequest(HttpMethods.GET, uri@Uri.Path("/api/guitar/inventory"), _, _, _) =>
+      val query = uri.query()
+      val inStockOption = query.get("inStock").map(_.toBoolean)
+
+      inStockOption match {
+        case Some(inStock) =>
+          val guitarsFuture: Future[List[Guitar]] = (guitarDb ? FindGuitarsInStock(inStock)).mapTo[List[Guitar]]
+          guitarsFuture.map { guitars =>
+            HttpResponse(
+              entity = HttpEntity(
+                ContentTypes.`application/json`,
+                guitars.toJson.prettyPrint
+              )
+            )
+          }
+        case None => Future(HttpResponse(StatusCodes.BadRequest))
+      }
+
+
+
+
+    case HttpRequest(HttpMethods.GET, uri@Uri.Path("/api/guitar"), _, _, _) =>
+      /*
+        query parameter handling code
+       */
+      val query = uri.query() // query object <=> Map[String, String]
+      if (query.isEmpty) {
+        val guitarsFuture: Future[List[Guitar]] = (guitarDb ? FindAllGuitars).mapTo[List[Guitar]]
+        guitarsFuture.map { guitars =>
+          HttpResponse(
+            entity = HttpEntity(
+              ContentTypes.`application/json`,
+              guitars.toJson.prettyPrint
+            )
           )
-        )
+        }
+      } else {
+        // fetch guitar associated to the guitar id
+        // localhost:8080/api/guitar?id=45
+        getGuitar(query)
       }
 
     case HttpRequest(HttpMethods.POST, Uri.Path("/api/guitar"), _, entity, _) =>
@@ -131,4 +223,11 @@ object LowLevelRest extends App with GuitarStoreJsonProtocol {
   }
 
   Http().bindAndHandleAsync(requestHandler, "localhost", 8080)
+
+  /**
+    * Exercise: enhance the Guitar case class with a quantity field, by default 0
+    * - GET to /api/guitar/inventory?inStock=true/false which returns the guitars in stock as a JSON
+    * V POST to /api/guitar/inventory?id=X&quantity=Y which adds Y guitars to the stock for guitar with id X
+    *
+    */
 }
